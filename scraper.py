f9518502a9e9c0c22fd0dff9dabca41700a01ad2
Peter Neish <peter.neish@gmail.com>
2014-11-14 16:02:50 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,12 +17,12 @@ root = lxml.html.fromstring(html)
 for li in root.cssselect("li[class='media-releases-item']"):
   title = li.cssselect("h4")[0].text_content()
   link  = url + li.cssselect("a")[0].attrib['href']
-  description = 
+  description = li.cssselect("p")[0].text_content()
   getdate = re.compile('News\/(.*)\/')
   date = getdate.match(link).group(1)
   date = dateutil.parser.parse(date)
   
   
   # # Write out to the sqlite database using scraperwiki library
-  scraperwiki.sqlite.save(unique_keys=['title'], data={"title": title, "link": link, "date": date})
+  scraperwiki.sqlite.save(unique_keys=['title'], data={"title": title, "link": link, "date": date, "description": description})
 
